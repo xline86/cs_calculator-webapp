c1aacff32f2df3ec3091ebdadca83d32c13958e3
roa-iris <roa86x@outlook.jp>
2025-06-05 01:49:40 +0900

    以下のファイルをgitignoreに追加

--- obj/Debug/net8.0/cs_calculator-webapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/cs_calculator-webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_calculator-webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77083cc6b0dfc320ad2f0aef8132675634841e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_calculator-webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_calculator-webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
